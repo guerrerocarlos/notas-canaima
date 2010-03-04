635c10bc8853806699ba068296d4a9497782dee3
Carlos Guerrero <guerrerocarlos@gmail.com>
2010-03-03 23:37:42 -0430

    primer commit

---
+++ hello/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from bienvenido.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    (r'^hello/$', hello),
+    (r'^date/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+)
