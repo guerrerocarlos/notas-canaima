ee2dd40ad091ef1bf1ee0b789e9f0d995a8e1bab
Carlos Guerrero <guerrerocarlos@gmail.com>
2010-03-17 20:42:58 -0430

    funciones basicas listas

--- canaima/notas/views.py
+++ canaima/notas/views.py
@@ -11,23 +11,26 @@ def bienvenido(request):
 
 def enviar(request,url):
     errors = []
+    notas_recientes = Nota.objects.order_by('-fecha')[0:7]
     if 'codigo_form' in request.POST:
         codigo_form = request.POST['codigo_form']
         titulo_form = request.POST['titulo_form']
         autor_form = request.POST['nombre_form']
-	a1=Autor(autor_form)
+	a1=Autor(name=autor_form)
 	a1.save()
         p1 = Nota(nota=codigo_form,titulo=titulo_form,autor=a1)
 	p1.save()
-        return render_to_response('1.html',{'exito': True})
+        return render_to_response('1.html',{'exito': True,'notas_recientes': notas_recientes})
     return render_to_response('1.html',{'errors': errors})
     
 
 def enviar2(request):
     errors = []
     if 'codigo_form' in request.POST:
-        codigo_form = request.POST['codigo_form']
-        return HttpResponse(codigo_form)
+       titulo_form = request.POST['titulo_form']
+       autor_form = request.POST['nombre_form']
+       codigo_form = request.POST['codigo_form']
+       return HttpResponse(codigo_form+titulo_form+autor_form)
     return HttpResponse("Error")
  
 
